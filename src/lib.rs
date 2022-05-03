a3685493eaaa25716ce56c588f03abf6eb80b872
Slawomir Mikulski <smikulsk@gmail.com>
2022-05-04 00:41:57 +0200

    Remove needless borrow

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use token_parser::parse;
 pub mod token_parser;
 
 pub fn evaluate(expression : &str) -> f64 {
-    let tokens = parse(&expression);
+    let tokens = parse(expression);
     println!("{:?}", tokens);
     0.0
 }
