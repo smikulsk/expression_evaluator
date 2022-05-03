8e7034205974e4ad029fdf50f8932fbb16654968
Slawomir Mikulski <smikulsk@gmail.com>
2022-05-03 23:23:14 +0200

    Implementation of token parser + tests

--- src/token_parser.rs
+++ src/token_parser.rs
@@ -1,20 +1,72 @@
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
-    Numeric,
+    Numeric(f64),
     Add,
     Subtract,
     Multiply,
     Divide,
     Power,
-    Sqrt      
+    Sqrt,
+    LeftBracket,
+    RightBracket,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Token {
-    pub token_type : TokenType   
+    pub token_type: TokenType,
 }
 
-pub fn parse(s : &str) -> Vec::<Token> {
-    let tokens = Vec::<Token>::new();
+pub fn parse(s: &str) -> Vec<Token> {
+    let mut tokens = vec![];
+    let mut current_token = String::new();
+    for ch in s.to_lowercase().chars() {
+        if ch.is_digit(10) || ch == '.' {
+            current_token.push(ch);
+        } else {
+            if !current_token.is_empty() && !["s".to_owned(), "sq".to_owned(), "sqr".to_owned()].contains(&current_token) {
+                tokens.push(Token {
+                    token_type: TokenType::Numeric(current_token.parse().unwrap()),
+                });
+                current_token = String::new();
+            }
+            match ch {
+                '+' => tokens.push(Token {
+                    token_type: TokenType::Add,
+                }),
+                '-' => tokens.push(Token {
+                    token_type: TokenType::Subtract,
+                }),
+                '*' => tokens.push(Token {
+                    token_type: TokenType::Multiply,
+                }),
+                '/' => tokens.push(Token {
+                    token_type: TokenType::Divide,
+                }),
+                '^' => tokens.push(Token {
+                    token_type: TokenType::Power,
+                }),
+                '(' => tokens.push(Token {
+                    token_type: TokenType::LeftBracket,
+                }),
+                ')' => tokens.push(Token {
+                    token_type: TokenType::RightBracket,
+                }),
+                's' => current_token.push(ch),
+                'q' => if current_token == "s" {
+                    current_token.push(ch);
+                },
+                'r' => if current_token == "sq" {
+                    current_token.push(ch);
+                },
+                't' => if current_token == "sqr" {
+                    tokens.push(Token {
+                        token_type: TokenType::Sqrt,
+                    });
+                    current_token = String::new();
+                },
+                _ => () //TODO: Add parsing error
+            }
+        }
+    }
     tokens
 }
--- src/token_parser_tests.rs
+++ src/token_parser_tests.rs
@@ -1,4 +1,4 @@
-use crate::token_parser::Token;
+use crate::token_parser::{Token, TokenType};
 
 use super::*;
 
@@ -8,6 +8,33 @@ pub fn parse_empty_string_returns_empty_list() {
     let given_expression = String::new();
 
     let tokens = parse(&given_expression);
+
+    assert_eq!(tokens, expected);
+}
+
+#[test]
+pub fn parse_non_empty_string_returns_correct_tokens() {
+    let expected = vec![
+        Token { token_type: TokenType::Numeric(123.0)},
+        Token { token_type: TokenType::Add},
+        Token { token_type: TokenType::Numeric(4.56)},
+        Token { token_type: TokenType::Multiply},
+        Token { token_type: TokenType::LeftBracket},
+        Token { token_type: TokenType::Sqrt},
+        Token { token_type: TokenType::LeftBracket},
+        Token { token_type: TokenType::Numeric(78.0)},
+        Token { token_type: TokenType::Divide},
+        Token { token_type: TokenType::Numeric(90.0)},
+        Token { token_type: TokenType::RightBracket},
+        Token { token_type: TokenType::Subtract},
+        Token { token_type: TokenType::Numeric(2.0)},
+        Token { token_type: TokenType::Power},
+        Token { token_type: TokenType::Numeric(12.0)},
+        Token { token_type: TokenType::RightBracket},
+    ];
+    let given_expression = "123+4.56*(sqrt(78/90)-2^12)";
+
+    let tokens = parse(given_expression);
 
     assert_eq!(tokens, expected);
 }
